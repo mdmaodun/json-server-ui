16b0dade8f533fb2a99c9aab2c2b9d55e6b690ac
mdmaodun <mdmaodun@sina.cn>
2020-11-22 16:27:47 +0800

    v1.1.0

--- README.md
+++ README.md
@@ -25,3 +25,25 @@ http://localhost:6660
 - 多数据库、集合管理
 - API 服务启动、停止
 - 端口检查
+- 支持中间件拦截
+  - 第一步，在 `/dbs/dbName/` 文件夹下添加 `middlewares` 文件夹
+  - 第二步，创建一个中间件文件，例如：`sayhi.js`
+    ```javascript
+    module.exports = (req, res, next) => {
+      // method: 请求方法
+      // url: 请求地址
+      // path: 请求路径，不包括查询参数
+      // query: 查询参数
+      // body: 请求体数据
+      // 注意：params没有，想要 params 路由参数请使用 url-pattern 第三方包解析
+      const { method, path, body, query } = req;
+      if (method === 'GET' && path === 'sayhi') {
+        const { name } = query;
+        res.send(`hi~${ name }!`);
+        return;
+      }
+      next();
+    }
+    ```
+  - 第三步，启动（如果事先已经启动，请停止后再启动），即可生效
+  - 第四步，访问路径 `/sayhi?name=王二麻子` 即可
--- server/utils/runjsonserver.js
+++ server/utils/runjsonserver.js
@@ -1,16 +1,26 @@
 const { spawn } = require('child_process');
 const isInUseOfPort = require('../utils/isInUseOfPort');
+const { readdirSync } = require('fs');
+const { dirname, join } = require('path');
 
 module.exports = ({ dbJsonFilePath, port }) => {
   return isInUseOfPort(port).then((isInUse) => {
     if (isInUse) {
       throw new Error(`This port:${port} is already occupied`);
     }
-    const childProcess = spawn('npx', ['json-server', '--port', port, '--watch', dbJsonFilePath], {
-      detached: true,
-      stdio: 'ignore',
-      windowsHide: true,
-    });
+
+    const middlewaresDirPath = join(dirname(dbJsonFilePath), 'middlewares');
+    const filePaths = readdirSync(middlewaresDirPath).map((v) => join(middlewaresDirPath, v));
+
+    const childProcess = spawn(
+      'npx',
+      ['json-server', '--port', port, '--middlewares', ...filePaths, '--watch', dbJsonFilePath],
+      {
+        detached: true,
+        stdio: 'ignore',
+        windowsHide: true,
+      }
+    );
     childProcess.unref();
     console.log(childProcess.pid, 'childProcess.pid');
     return childProcess;
